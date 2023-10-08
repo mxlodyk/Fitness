b200242b23d1ae1e593942af8fe9c92d506576ff
Melody Flavel <melodykimfl@gmail.com>
2023-10-08 11:39:49 +0800

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Melody Flavel on 8/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
